92114b2c53ea1c81cd9a067ccefc6962ef620f8c
Farzeensiddiquie <farzeenwasif15@gmail.com>
2026-02-17 22:51:37 +0500

    commity

--- components/RobotCanvas.js
+++ components/RobotCanvas.js
@@ -48,22 +48,13 @@ function RobotModel({ url }) {
   return <primitive ref={group} object={scene} position={[0, -1.1, 0]} />;
 }
 
-// Optional fallback loader (in case of very slow network)
-function Loader() {
-  return (
-    <div className="flex items-center justify-center h-full text-white text-sm font-light animate-pulse">
-      Loading 3D Model...
-    </div>
-  );
-}
-
 export default function RobotCanvas() {
   return (
     <div className="md:w-[40vw] w-[80vw] md:h-[80vh] h-[70vh] bg-transparent">
       <Canvas camera={{ position: [0, 0, 6], fov: 45 }}>
         <ambientLight intensity={0.8} />
         <directionalLight position={[5, 5, 5]} intensity={1.2} />
-        <Suspense fallback={<Loader />}>
+        <Suspense fallback={null}>
           <RobotModel url="/futuristic_flying_animated_robot_-_low_poly.glb" />
         </Suspense>
       </Canvas>
